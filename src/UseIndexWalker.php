d0f7e4d443cd5f2203b0d9107b00019279a8b233
github-ci <you@example.com>
2025-04-25 14:53:50 +0000

    feat(doctrine-use-index-walker): 添加 Doctrine 使用索引查询器包

---
+++ src/UseIndexWalker.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tourze\DoctrineUseIndexWalker;
+
+use Doctrine\DBAL\Platforms\AbstractMySQLPlatform;
+use Doctrine\ORM\Query\SqlWalker;
+
+/**
+ * Quick hack to allow adding a USE INDEX on the query
+ */
+class UseIndexWalker extends SqlWalker
+{
+    public const HINT_USE_INDEX = 'UseIndexWalker.UseIndex';
+
+    public const HINT_FORCE_INDEX = 'UseIndexWalker.ForceIndex';
+
+    public function walkFromClause($fromClause): string
+    {
+        $sql = parent::walkFromClause($fromClause);
+
+        // 这个目前只有MySQL支持
+        if ($this->getConnection()->getDatabasePlatform() instanceof AbstractMySQLPlatform) {
+            if ($index = $this->getQuery()->getHint(self::HINT_USE_INDEX)) {
+                return preg_replace('#(\bFROM\s*\w+\s*\w+)#', '\1 USE INDEX (' . $index . ')', $sql);
+            }
+
+            if ($index = $this->getQuery()->getHint(self::HINT_FORCE_INDEX)) {
+                return preg_replace('#(\bFROM\s*\w+\s*\w+)#', '\1 FORCE INDEX (' . $index . ')', $sql);
+            }
+        }
+
+        return $sql;
+    }
+}
